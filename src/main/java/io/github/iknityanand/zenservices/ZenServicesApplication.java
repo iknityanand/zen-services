85d384d44b76a47f9df9f7082c2645bf4408a51d
iknityanand <iknityanand@gmail.com>
2019-12-08 22:58:30 +0530

    inital commit

---
+++ src/main/java/io/github/iknityanand/zenservices/ZenServicesApplication.java
@@ -0,0 +1,13 @@
+package io.github.iknityanand.zenservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/iknityanand/zenservices/ZenServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iknityanand.zenservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
